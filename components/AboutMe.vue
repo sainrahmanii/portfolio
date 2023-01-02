31f41a411303c6bacbe0b95b1003cb5fe742e461
sainrahmanii <husainrahmani127@gmail.com>
2023-01-02 12:04:57 +0700

    add responsive navbar and aos

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -1,14 +1,44 @@
 <template>
   <section class="pt-36">
     <div class="container lg:px-36">
-      <div class="rounded-lg bg-gray-50 shadow-xl">
-        <div class="py-3">
-          <h1 class="text-center text-3xl">About Me</h1>
-          <p class="text-center font-medium mt-2">
-            Junior Web Developer dengan pengalaman 1+ tahun lebih berkarir sebagai web developer, memiliki kemampuan yang baik dalam pengembangan web menggunakan framework Laravel sebagai backend, MySql sebagai database, Git dan Github sebagai Version Control System, framework Vue.Js dan Nuxt.Js sebagai frontend. Sedang mencari pekerjaan untuk menjadi Freelance Web Developer.
-          </p>
+      <div class="flex flex-wrap items-center my-12">
+        <div
+          class="w-full items-center lg:w-1/2"
+          data-aos="fade-up"
+          data-aos-delay="50"
+          data-aos-duration="1000"
+          data-aos-easing="ease-in-out"
+          data-aos-mirror="true"
+          data-aos-once="false"
+        >
+          <img
+            src="../assets/images/husain.png"
+            alt="husain rahmani"
+            class="lg:max-w-full w-2/3 lg:mx-0 mx-auto"
+          />
+        </div>
+        <div class="w-full lg:w-1/2">
+          <div class="rounded-lg shadow-xl">
+            <div class="py-3">
+              <h1 class="text-center text-3xl">About Me</h1>
+              <p class="text-center font-medium mt-2">
+                Junior Web Developer dengan pengalaman 1+ tahun lebih berkarir
+                sebagai web developer, memiliki kemampuan yang baik dalam
+                pengembangan web menggunakan framework Laravel sebagai backend,
+                MySql sebagai database, Git dan Github sebagai Version Control
+                System, framework Vue.Js dan Nuxt.Js sebagai frontend. Sedang
+                mencari pekerjaan untuk menjadi Freelance Web Developer.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+
+<script setup>
+import AOS from 'aos'
+
+AOS.init()
+</script>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,3 +1,124 @@
 <template>
-
+  <div>
+    <header
+      class="bg-transparent absolute top-0 left-0 w-full flex items-center z-10 lg:px-36 navbar-fixed"
+    >
+      <div class="container">
+        <div class="flex items-center justify-between relative">
+          <div class="px-4">
+            <router-link
+              to="/"
+              class="font-bold text-lg text-cyan-500 py-6 flex items-center"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-6 h-6 mr-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.25 3v1.5M4.5 8.25H3m18 0h-1.5M4.5 12H3m18 0h-1.5m-15 3.75H3m18 0h-1.5M8.25 19.5V21M12 3v1.5m0 15V21m3.75-18v1.5m0 15V21m-9-1.5h10.5a2.25 2.25 0 002.25-2.25V6.75a2.25 2.25 0 00-2.25-2.25H6.75A2.25 2.25 0 004.5 6.75v10.5a2.25 2.25 0 002.25 2.25zm.75-12h9v9h-9v-9z"
+                />
+              </svg>
+              Husain Rahmani
+            </router-link>
+          </div>
+          <div class="flex items-center px-4">
+            <button
+              @click="mobileMenu = !mobileMenu"
+              class="block absolute right-4 lg:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-5 h-5"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
+                />
+              </svg>
+            </button>
+            <nav
+              id="nav-menu"
+              class="absolute py-5 bg-white shadow-lg rounded-lg max-w-[360px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none"
+              :class="{ hidden: mobileMenu }"
+            >
+              <ul class="block lg:flex">
+                <li class="group">
+                  <a
+                    href="#home"
+                    class="text-base text-slate-700 font-medium py-2 mx-8 group-hover:text-gray-700 flex"
+                    >Home</a
+                  >
+                </li>
+                <li class="group">
+                  <a
+                    href="#about"
+                    class="text-base text-slate-700 font-medium py-2 mx-8 group-hover:text-gray-700 flex"
+                    >About</a
+                  >
+                </li>
+                <li class="group">
+                  <a
+                    href="#portfolio"
+                    class="text-base text-slate-700 font-medium py-2 mx-8 group-hover:text-gray-700 flex"
+                    >Portfolio</a
+                  >
+                </li>
+                <li class="group">
+                  <a
+                    href="#clients"
+                    class="text-base text-slate-700 font-medium py-2 mx-8 group-hover:text-gray-700 flex"
+                    >Client</a
+                  >
+                </li>
+                <li class="group">
+                  <a
+                    href="#contact"
+                    class="text-base text-slate-700 font-medium py-2 mx-8 group-hover:text-gray-700 flex"
+                    >Contact</a
+                  >
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
+
+<style scoped>
+.navbar-fixed {
+  @apply fixed z-[9999] bg-white bg-opacity-70;
+  backdrop-filter: blur(5px);
+  box-shadow: inset 0 -1px 0 0 rgba(0, 0, 0, 0.1);
+}
+</style>
+
+<script setup>
+import { ref } from 'vue'
+window.onscroll = function () {
+  const navbar = document.querySelector('header')
+  const fixedNav = navbar.offsetTop
+
+  if (window.pageYOffset > fixedNav) {
+    navbar.classList.add('navbar-fixed')
+    navbar.classList.add('shadow-md')
+  } else {
+    navbar.classList.remove('navbar-fixed')
+    navbar.classList.remove('shadow-md')
+  }
+}
+
+const mobileMenu = ref(true)
+</script>
