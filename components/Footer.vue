4d171489eed1dce37477057bc2925e5bf6f3dcbc
sainrahmanii <husainrahmani127@gmail.com>
2023-01-04 00:10:43 +0700

    change text size

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer>
     <div class="bottom-0 left-0 right-0 w-full py-3 bg-cyan-500">
-      <h2 class="text-center text-white">
+      <h2 class="text-center text-white text-xs lg:text-sm">
         &copy; All Rights Reserved ● Develop by
         <a
           href="https://www.instagram.com/husain.rahmani12/"
