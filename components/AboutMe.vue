6fee4fe92b0b5a664d877d735031613aa145d42e
sainrahmanii <husainrahmani127@gmail.com>
2023-01-03 22:29:13 +0700

    fixed aos and aboutme

--- components/AboutMe.vue
+++ components/AboutMe.vue
@@ -14,21 +14,258 @@
           <img
             src="../assets/images/husain.png"
             alt="husain rahmani"
-            class="lg:max-w-full w-2/3 mx-auto"
+            class="lg:max-w-full w-64 mx-auto lg:mx-3"
           />
         </div>
         <div class="w-full items-center lg:w-1/2">
           <div class="rounded-lg shadow-xl">
             <div class="py-3">
-              <h1 class="text-center text-3xl">About Me</h1>
-              <p class="text-center font-medium mt-2">
-                Junior Web Developer dengan pengalaman 1+ tahun lebih berkarir
-                sebagai web developer, memiliki kemampuan yang baik dalam
-                pengembangan web menggunakan framework Laravel sebagai backend,
-                MySql sebagai database, Git dan Github sebagai Version Control
-                System, framework Vue.Js dan Nuxt.Js sebagai frontend. Sedang
-                mencari pekerjaan untuk menjadi Freelance Web Developer.
-              </p>
+              <h1 class="text-center text-3xl font-bold">About Me</h1>
+              <div class="mt-2">
+                <div
+                  class="collapse"
+                  data-aos="fade-up"
+                  data-aos-delay="50"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="false"
+                >
+                  <input type="checkbox" />
+                  <div class="flex items-center collapse-title">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="w-6 h-6 fill-cyan-500"
+                    >
+                      <path
+                        d="M11.25 4.533A9.707 9.707 0 006 3a9.735 9.735 0 00-3.25.555.75.75 0 00-.5.707v14.25a.75.75 0 001 .707A8.237 8.237 0 016 18.75c1.995 0 3.823.707 5.25 1.886V4.533zM12.75 20.636A8.214 8.214 0 0118 18.75c.966 0 1.89.166 2.75.47a.75.75 0 001-.708V4.262a.75.75 0 00-.5-.707A9.735 9.735 0 0018 3a9.707 9.707 0 00-5.25 1.533v16.103z"
+                      />
+                    </svg>
+
+                    <h1 class="pl-5 text-xl font-semibold">Experience</h1>
+                  </div>
+                  <div class="flex collapse-content font-medium">
+                    <p class="pl-2 text-justify">
+                      1+ years career as web developer, have good skills in web
+                      development
+                    </p>
+                  </div>
+                </div>
+                <div
+                  class="collapse -mt-3"
+                  data-aos="fade-up"
+                  data-aos-delay="100"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="false"
+                >
+                  <input type="checkbox" />
+                  <div class="flex items-center collapse-title">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="w-6 h-6 fill-violet-500"
+                    >
+                      <path
+                        d="M11.644 1.59a.75.75 0 01.712 0l9.75 5.25a.75.75 0 010 1.32l-9.75 5.25a.75.75 0 01-.712 0l-9.75-5.25a.75.75 0 010-1.32l9.75-5.25z"
+                      />
+                      <path
+                        d="M3.265 10.602l7.668 4.129a2.25 2.25 0 002.134 0l7.668-4.13 1.37.739a.75.75 0 010 1.32l-9.75 5.25a.75.75 0 01-.71 0l-9.75-5.25a.75.75 0 010-1.32l1.37-.738z"
+                      />
+                      <path
+                        d="M10.933 19.231l-7.668-4.13-1.37.739a.75.75 0 000 1.32l9.75 5.25c.221.12.489.12.71 0l9.75-5.25a.75.75 0 000-1.32l-1.37-.738-7.668 4.13a2.25 2.25 0 01-2.134-.001z"
+                      />
+                    </svg>
+
+                    <h1 class="pl-5 text-xl font-semibold">Skills</h1>
+                  </div>
+                  <div class="flex collapse-content font-medium -mt-3">
+                    <div class="text-justify">
+                      <div class="collapse">
+                        <input type="checkbox" />
+                        <div
+                          class="font-semibold collapse-title flex items-center"
+                        >
+                          FUNDAMENTAL
+                        </div>
+                        <div class="flex items-center collapse-content -mt-3">
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://img.icons8.com/external-tal-revivo-tritone-tal-revivo/32/null/external-html-5-is-a-software-solution-stack-that-defines-the-properties-and-behaviors-of-web-page-logo-tritone-tal-revivo.png"
+                              class="mr-2"
+                            />
+                            <p>HTML5</p>
+                          </div>
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://img.icons8.com/fluency/32/null/css3.png"
+                            />
+                            <p>CSS3</p>
+                          </div>
+                          <div class="flex items-center">
+                            <img
+                              src="https://img.icons8.com/fluency/32/null/javascript.png"
+                              class="mr-2"
+                            />
+                            <p>JavaScript</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="collapse -mt-7">
+                        <input type="checkbox" />
+                        <h1 class="font-semibold collapse-title">
+                          FRAMEWORK CSS
+                        </h1>
+                        <div class="flex items-center collapse-content -mt-3">
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/800px-Bootstrap_logo.svg.png"
+                              class="mr-2 w-7"
+                            />
+                            <p>Bootstrap</p>
+                          </div>
+                          <div class="flex items-center">
+                            <img
+                              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png"
+                              class="mr-2 w-7"
+                            />
+                            <p>Tailwind</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="collapse -mt-7">
+                        <input type="checkbox" />
+                        <h1 class="font-semibold collapse-title">
+                          FRAMEWORK FRONTEND
+                        </h1>
+                        <div class="flex items-center collapse-content -mt-3">
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://docs.vuejs.id/images/logo.png"
+                              class="mr-2 w-6"
+                            />
+                            <p>Vue.Js</p>
+                          </div>
+                          <div class="flex items-center">
+                            <img
+                              src="../assets/images/nuxt.png"
+                              class="mr-2 w-7"
+                            />
+                            <p>Nuxt.Js</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="collapse -mt-7">
+                        <input type="checkbox" />
+                        <h1 class="font-semibold collapse-title">
+                          FRAMEWORK BACKEND
+                        </h1>
+                        <div class="flex items-center collapse-content -mt-3">
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/1200px-Laravel.svg.png"
+                              class="mr-2 w-6"
+                            />
+                            <p>Laravel</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="collapse -mt-7">
+                        <input type="checkbox" />
+                        <h1 class="font-semibold collapse-title">DATABASE</h1>
+                        <div class="flex items-center collapse-content -mt-3">
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://upload.wikimedia.org/wikipedia/id/a/a9/MySQL.png"
+                              class="mr-2 w-10"
+                            />
+                            <p>MySQL</p>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="collapse -mt-7">
+                        <input type="checkbox" />
+                        <h1 class="font-semibold collapse-title">
+                          VCS (Version Control System)
+                        </h1>
+                        <div class="flex items-center collapse-content -mt-3">
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://avatars.githubusercontent.com/u/18133?s=200&v=4"
+                              class="mr-2 w-8"
+                            />
+                            <p>Git</p>
+                          </div>
+                          <div class="flex items-center mr-5">
+                            <img
+                              src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                              class="mr-2 w-10"
+                            />
+                            <p>Github</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="collapse -mt-3"
+                  data-aos="fade-up"
+                  data-aos-delay="150"
+                  data-aos-duration="1000"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="false"
+                >
+                  <input type="checkbox" />
+                  <div class="flex items-center collapse-title">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      class="w-6 h-6 fill-teal-500"
+                    >
+                      <path
+                        d="M11.7 2.805a.75.75 0 01.6 0A60.65 60.65 0 0122.83 8.72a.75.75 0 01-.231 1.337 49.949 49.949 0 00-9.902 3.912l-.003.002-.34.18a.75.75 0 01-.707 0A50.009 50.009 0 007.5 12.174v-.224c0-.131.067-.248.172-.311a54.614 54.614 0 014.653-2.52.75.75 0 00-.65-1.352 56.129 56.129 0 00-4.78 2.589 1.858 1.858 0 00-.859 1.228 49.803 49.803 0 00-4.634-1.527.75.75 0 01-.231-1.337A60.653 60.653 0 0111.7 2.805z"
+                      />
+                      <path
+                        d="M13.06 15.473a48.45 48.45 0 017.666-3.282c.134 1.414.22 2.843.255 4.285a.75.75 0 01-.46.71 47.878 47.878 0 00-8.105 4.342.75.75 0 01-.832 0 47.877 47.877 0 00-8.104-4.342.75.75 0 01-.461-.71c.035-1.442.121-2.87.255-4.286A48.4 48.4 0 016 13.18v1.27a1.5 1.5 0 00-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.661a6.729 6.729 0 00.551-1.608 1.5 1.5 0 00.14-2.67v-.645a48.549 48.549 0 013.44 1.668 2.25 2.25 0 002.12 0z"
+                      />
+                      <path
+                        d="M4.462 19.462c.42-.419.753-.89 1-1.394.453.213.902.434 1.347.661a6.743 6.743 0 01-1.286 1.794.75.75 0 11-1.06-1.06z"
+                      />
+                    </svg>
+
+                    <h1 class="pl-5 text-xl font-semibold">Education</h1>
+                  </div>
+                  <div class="flex collapse-content font-medium -mt-3">
+                    <div class="text-justify">
+                      <div class="collapse">
+                        <input type="checkbox" />
+                        <h1 class="font-semibold collapse-title">
+                          STIKOM CKI JAKARTA ● 2021 - Present
+                        </h1>
+                        <div class="flex items-center collapse-content -mt-5">
+                          <p>Semester 3</p>
+                        </div>
+                      </div>
+                      <div class="collapse -mt-7">
+                        <input type="checkbox" />
+                        <h1 class="font-semibold collapse-title">
+                          MA'HAD DAARUT TAUHID ● 2014 - 2020
+                        </h1>
+                        <div class="flex items-center collapse-content -mt-5">
+                          <p>Graduted</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
