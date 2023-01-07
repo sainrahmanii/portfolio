9b65291d143c8c8b29d667dfc24a3f7f0a04bd25
sainrahmanii <husainrahmani127@gmail.com>
2023-01-07 14:06:52 +0700

    add link

--- components/Contact.vue
+++ components/Contact.vue
@@ -4,20 +4,20 @@
       <h2 class="text-3xl font-bold text-center">CONTACT</h2>
       <div class="mt-5 lg:flex gap-5 md:block">
         <div class="flex items-center">
-          <img src="https://img.icons8.com/cute-clipart/50/null/instagram-new.png"/>
-          <h2 class="hover:underline text-xl font-medium">husain.rahmani12</h2>
+          <img src="https://img.icons8.com/cute-clipart/30/null/instagram-new.png"/>
+          <a href="https://www.instagram.com/husain.rahmani12/"><h2 class="hover:underline text-xl font-medium ml-1">husain.rahmani12</h2></a>
         </div>
         <div class="flex items-center">
-          <img src="https://img.icons8.com/color/50/null/whatsapp--v1.png"/>
-          <h2 class="hover:underline text-xl font-medium">085213221817</h2>
+          <img src="https://img.icons8.com/color/30/null/whatsapp--v1.png"/>
+          <a href="https://wa.me/085213221817?text=Hi,%20Husain%20Rahmani%0ASaya%20ingin%20membuat%20website"><h2 class="hover:underline text-xl font-medium ml-1">085213221817</h2></a>
         </div>
         <div class="flex items-center">
-          <img src="https://img.icons8.com/color/48/null/gmail-new.png"/>
-          <h2 class="ml-2 hover:underline text-xl font-medium">husainrahmani127@gmail.com</h2>
+          <img src="https://img.icons8.com/color/30/null/gmail-new.png"/>
+          <a href="mailto:husainrahmani127@gmail.com"><h2 class="hover:underline text-xl font-medium ml-1">husainrahmani127@gmail.com</h2></a>
         </div>
         <div class="flex items-center">
-          <img src="https://img.icons8.com/glyph-neue/50/null/github.png"/>
-          <h2 class="hover:underline text-xl font-medium">sainrahmanii</h2>
+          <img src="https://img.icons8.com/glyph-neue/30/null/github.png"/>
+          <a href="https://github.com/sainrahmanii"><h2 class="hover:underline text-xl font-medium ml-1">sainrahmanii</h2></a>
         </div>
       </div>
     </div>
